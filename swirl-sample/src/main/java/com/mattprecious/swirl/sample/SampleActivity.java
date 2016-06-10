98ff26989f6ce36fba6de0fd7f5060604b316217
Matt Precious <mattp@squareup.com>
2016-06-10 10:27:44 -0400

    Min SDK 21 and other version updates

--- swirl-sample/src/main/java/com/mattprecious/swirl/sample/SampleActivity.java
+++ swirl-sample/src/main/java/com/mattprecious/swirl/sample/SampleActivity.java
@@ -2,15 +2,17 @@ package com.mattprecious.swirl.sample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
 import android.widget.RadioGroup;
-import butterknife.Bind;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.mattprecious.swirl.SwirlView;
 
 public class SampleActivity extends Activity
     implements RadioGroup.OnCheckedChangeListener {
-  @Bind(R.id.swirl) SwirlView swirlView;
-  @Bind(R.id.state) RadioGroup stateView;
+  @BindView(R.id.swirl) SwirlView swirlView;
+  @BindView(R.id.state) RadioGroup stateView;
+  @BindView(R.id.animate) CompoundButton animateView;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -23,13 +25,13 @@ public class SampleActivity extends Activity
   @Override public void onCheckedChanged(RadioGroup group, int checkedId) {
     switch (checkedId) {
       case R.id.off:
-        swirlView.setState(SwirlView.State.OFF);
+        swirlView.setState(SwirlView.State.OFF, animateView.isChecked());
         break;
       case R.id.on:
-        swirlView.setState(SwirlView.State.ON);
+        swirlView.setState(SwirlView.State.ON, animateView.isChecked());
         break;
       case R.id.error:
-        swirlView.setState(SwirlView.State.ERROR);
+        swirlView.setState(SwirlView.State.ERROR, animateView.isChecked());
         break;
       default:
         throw new IllegalArgumentException("Unexpected checkedId: " + checkedId);
